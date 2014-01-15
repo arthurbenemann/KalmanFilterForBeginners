2b6e6bb760eba1b666e98f03990321d9b1272a1c
Arthur Benemann <arthur.benemann@gmail.com>
2014-01-14 22:42:14 -0200

    Writing the test code of chapter 1.

---
+++ TestAvgFilter.m
@@ -0,0 +1,22 @@
+clear all
+
+dt = 0.2;
+t = 0:dt:10;
+
+Nsamples = length(t);
+
+Xmsaved = zeros(Nsamples, 1);
+Avgsaved = zeros(Nsamples, 1);
+
+for k=1:Nsamples
+	xm = GetVolt();
+	avg = AvgFilter(xm);
+	
+	Avgsaved(k) = avg; 
+	Xmsaved(k) = xm;
+end
+
+figure
+plot(t, Xmsaved, 'r:*')
+hold on
+plot(t, Avgsaved, 'o-')
