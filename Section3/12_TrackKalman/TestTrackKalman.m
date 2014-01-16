3f0e9ba5c6199d21149de7993d4d6cc9df56fdd7
Arthur Benemann <arthur.benemann@gmail.com>
2014-01-16 03:49:22 -0200

    Finished example 12.3

---
+++ Section3/12_TrackKalman/TestTrackKalman.m
@@ -0,0 +1,30 @@
+pkg load general control specfun signal image % Check the README file for how to get this librarys
+
+clear all
+
+NoOfImg = 24;
+
+Xmsaved = zeros(2,NoOfImg);
+Xhsaved = zeros(2,NoOfImg);
+
+figure 					% added to start with a clean figure
+for k=1:NoOfImg
+  [xm, ym] = GetBallPos(k);
+  [xh, yh] = TrackKalman(xm, ym);
+      
+  hold on
+  plot(xm, ym, 'r*')
+  plot(xh, yh, 'bs')
+  
+  pause(0.5)				% 1s iteration was too slow
+  
+  Xmsaved(:, k) = [xm ym]';
+  Xhsaved(:, k) = [xh yh]';
+
+end
+
+figure
+hold on
+plot(Xmsaved(1,:), Xmsaved(2,:), '*')
+plot(Xhsaved(1,:), Xhsaved(2,:), 's')
+axis ij					% makes the plot axis match the image
---
+++ Section3/12_TrackKalman/TrackKalman.m
@@ -0,0 +1,41 @@
+function [xh yh] = TrackKalman(xm,ym)
+%
+%
+persistent A H Q R
+persistent x P
+persistent firstRun
+
+%--- Initial Conditions
+if isempty(firstRun)
+	firstRun = 1;
+
+	dt = 1;
+
+	A = [1  dt 0  0 	% System Model
+	     0  1  0  0
+             0  0  1  dt
+             0  0  0  1 ];	
+	H = [1 0 0 0		% Measurament Model
+	     0 0 1 0 ];
+
+	Q = 1.0*eye(4);		% Covariance matrix of state transition noise 
+	R = [50 0		% Covariance matrix of measurament noise
+             0  50];
+
+	x = [0, 0, 0, 0]';	% Guess of the initial state of the system
+	P = 100*eye(4);		% Covariance of the estimative above
+end
+
+%--- Prediction
+xp = A*x;			% State prediction
+Pp = A*P*A'+ Q;			% Error covariance prediction
+
+%--- Estimation
+K = Pp*H'*inv(H*Pp*H' + R); 	% Kalman gain
+
+z = [xm ym]';
+x = xp + K*(z - H*xp);		% State estimation
+P = Pp - K*H*Pp;		% Error covariance estimation
+
+xh = x(1);
+yh = x(3);
