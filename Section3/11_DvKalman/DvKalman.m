a4b3159ff8fa95cfdf49e59c2aec967faabc5113
Arthur Benemann <arthur.benemann@gmail.com>
2014-01-16 01:00:54 -0200

    Finished chapter 11.3

---
+++ Section3/11_DvKalman/DvKalman.m
@@ -0,0 +1,35 @@
+function [pos vel] = DvKalman(z)
+%
+%
+persistent A H Q R
+persistent x P
+persistent firstRun
+
+%--- Initial Conditions
+if isempty(firstRun)
+	firstRun = 1;
+
+	dt = 0.1;
+
+	A = [1 dt; 0 1];	% System Model
+	H = [1 0];		% Measurament Model
+
+	Q = [1 0; 0 3];		% Covariance matrix of state transition noise 
+	R = 10;			% Covariance matrix of measurament noise
+
+	x = [0 20]';		% Guess of the initial state of the system
+	P = 5*eye(2);		% Covariance of the estimative above
+end
+
+%--- Prediction
+xp = A*x;			% State prediction
+Pp = A*P*A'+ Q;			% Error covariance prediction
+
+%--- Estimation
+K = Pp*H'*inv(H*Pp*H' + R); 	% Kalman gain
+
+x = xp + K*(z - H*xp);		% State estimation
+P = Pp - K*H*Pp;		% Error covariance estimation
+
+pos = x(1);
+vel = x(2);
